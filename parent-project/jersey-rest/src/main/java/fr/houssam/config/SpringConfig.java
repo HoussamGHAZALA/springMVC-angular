01324b7dbb09f32c0e0e7e54c290605ebdd93655
Houssam GHAZALA <houssem.ghazala@gmail.com>
2015-05-22 23:03:14 +0200

    [springMVC-angular] Nettoyage des modules maven.

--- parent-project/jersey-rest/src/main/java/fr/houssam/config/SpringConfig.java
+++
@@ -1,9 +0,0 @@
-package fr.houssam.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration 
-@ComponentScan(basePackages = "com.houssam")
-public class SpringConfig {
-}
